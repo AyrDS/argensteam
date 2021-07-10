ea069e6e10906c6437e898bda035d87715ede41d
AyrDS <ayrtonds97@gmail.com>
2021-07-10 04:42:20 -0300

    Agregando carrito de compras al proyecto final

--- script.js
+++ script.js
@@ -18,8 +18,28 @@ const productos = [{
 const codigos = ["ABC-123", "DFG-456", "BSD-741", "Lo siento, no hubo suerte", "Lo siento, no hubo suerte", "Lo siento, no hubo suerte", "Lo siento, no hubo suerte", "Lo siento, no hubo suerte", "Lo siento, no hubo suerte"];
 
 let carrito = {};
-const btnCarrito = document.getElementsByClassName("carrito_boton")
+
+document.addEventListener("DOMContentLoaded", () => {
+    //Obtenemos el modo actual.
+    if (localStorage.getItem("darkMode") === "true") {
+        document.body.classList.add("dark");
+        btnSwitch.classList.add("activeDM");
+    } else {
+        document.body.classList.remove("dark");
+        btnSwitch.classList.remove("activeDM");
+    }
+
+    //Obtenemos el carrito
+    if(localStorage.getItem("carrito")){
+        carrito = JSON.parse(localStorage.getItem("carrito"))
+        mostrarCarrito();
+    }
+})
+
+const btnCarrito = document.getElementsByClassName("carrito_boton");
+const carritoProductos = document.getElementById("carrito-productos");
 const templateCarrito = document.getElementById("template_carrito").content;
+const templateFoot = document.getElementById("template__foot").content;
 const catalogo = document.getElementById("catalogo");
 const btnSwitch = document.getElementById("switch");
 const btnSuerte = document.getElementById("suerte");
@@ -27,6 +47,15 @@ const btnCerrarPopup = document.getElementById("cerrar__popUp");
 const overlay = document.getElementById("overlay");
 const popup = document.getElementById("popUp");
 const contenedorPopup = document.createElement("p");
+const fragment = document.createDocumentFragment();
+
+catalogo.addEventListener("click", e => {
+    agregarCarrito(e);
+});
+
+carritoProductos.addEventListener("click", e => {
+    eventoBtn(e);
+});
 
 /* CATALOGO */
 for (const producto of productos) {
@@ -35,7 +64,7 @@ for (const producto of productos) {
     contenedor.innerHTML =
         `<img src="img/productos/${producto.id}.jpg" class="portada mx-auto d-block">
         <p class="mt-3">${producto.nombre}</p>
-        <p>$${producto.precio}</p>
+        <p>$<span>${producto.precio}</span></p>
         <button type="button" data-id="${producto.id}" class="btn btn-outline-success btn-comprar mx-auto d-block">Comprar</button>`;
 
     contenedor.classList.add("col", "text-center", "producto", "mb-5", producto.categoria);
@@ -63,15 +92,6 @@ btnSwitch.addEventListener("click", function () {
         localStorage.setItem("darkMode", "false");
     }
 });
-
-//Obtenemos el modo actual.
-if (localStorage.getItem("darkMode") === "true") {
-    document.body.classList.add("dark");
-    btnSwitch.classList.add("activeDM");
-} else {
-    document.body.classList.remove("dark");
-    btnSwitch.classList.remove("activeDM");
-}
 /* ---------------------- */
 
 /* BOTON SUERTE */
@@ -96,13 +116,9 @@ btnCerrarPopup.addEventListener("click", () => {
 
 /* ----------------------------- */
 
+
 /* AGREGAR CARRITO */
-catalogo.addEventListener("click", e => {
-    agregarCarrito(e);
-});
-
 const agregarCarrito = e => {
-
     if (e.target.classList.contains("btn-comprar")) {
         setCarrito(e.target.parentElement);
     }
@@ -110,11 +126,12 @@ const agregarCarrito = e => {
 }
 
 const setCarrito = objeto => {
+
     const producto = {
         id: objeto.querySelector(".btn-comprar").dataset.id,
         imagen: objeto.querySelector("img").src,
         nombre: objeto.querySelectorAll("p")[0].textContent,
-        precio: objeto.querySelectorAll("p")[1].textContent,
+        precio: objeto.querySelector("span").textContent,
         cantidad: 1
     }
 
@@ -122,14 +139,71 @@ const setCarrito = objeto => {
         producto.cantidad = carrito[producto.id].cantidad + 1;
     }
     carrito[producto.id] = {...producto};
-    console.log(carrito);
+    mostrarCarrito();
 }
 
-const mostrarCarrito = () =>{
-    mostrarCarrito();
-    Object.values(carrito).forEach(producto =>{
+const mostrarCarrito = () => {
+
+    carritoProductos.innerHTML = ``;
+    Object.values(carrito).forEach(producto => {
         templateCarrito.querySelector("img").src = producto.imagen;
         templateCarrito.querySelectorAll("p")[0].textContent = producto.nombre;
-        templateCarrito.que
+        templateCarrito.querySelectorAll("span")[0].textContent = producto.precio;
+        templateCarrito.querySelectorAll("span")[1].textContent = producto.cantidad;
+        templateCarrito.querySelector(".btn-info").dataset.id = producto.id;
+        templateCarrito.querySelector(".btn-danger").dataset.id = producto.id;
+
+        const clone = templateCarrito.cloneNode(true);
+        fragment.appendChild(clone);
     })
+    carritoProductos.appendChild(fragment);
+    mostrarFoot();
+
+    localStorage.setItem("carrito", JSON.stringify(carrito));
+}
+
+const mostrarFoot = () => {
+
+    if (Object.keys(carrito).length === 0) {
+        carritoProductos.innerHTML = `<p> No tienes productos agregados </p>`;
+
+        return;
+    }
+
+    const nPrecio = Object.values(carrito).reduce((acumulador, {cantidad, precio}) => acumulador + cantidad * precio, 0);
+
+    templateFoot.querySelector("span").textContent = nPrecio;
+
+    const clone = templateFoot.cloneNode(true);
+    fragment.appendChild(clone);
+    carritoProductos.appendChild(fragment);
+    /* VACIAR CARRO */
+    const btnVaciar = document.getElementById("vaciar-carrito");
+    btnVaciar.addEventListener("click", () => {
+        carrito = {};
+        mostrarCarrito();
+    })
+}
+/* ------------------------------------ */
+
+const eventoBtn = e => {
+    //Aumentar
+    if (e.target.classList.contains("btn-info")) {
+
+        const producto = carrito[e.target.dataset.id];
+        producto.cantidad++;
+        carrito[e.target.dataset.id] = {...producto};
+        mostrarCarrito();
+    }
+    //Restar productos
+    if (e.target.classList.contains("btn-danger")) {
+        const producto = carrito[e.target.dataset.id];
+        producto.cantidad--;
+        if (producto.cantidad === 0) {
+            delete carrito[e.target.dataset.id]
+        }
+        mostrarCarrito();
+    }
+
+    e.stopPropagation();
 }
